54bab6d3f291f59cb202c6fa1ee132cdc8087f59
Tobias Wåle <tobias.g.wale@nmbu.no>
2026-01-15 12:45:02 +0100

    video.py, Enhance docstring for video generation script to improve clarity and detail. Include usage information and description of parameters. ref #5

--- src/video.py
+++ src/video.py
@@ -1,8 +1,13 @@
 import cv2
+"""
+Generate a video from simulation images stored in the tmp directory.
 
-n_frames = 250
-img_template = "tmp/img_{:04d}.png"
-output_video = "video.avi"
+Uses opencv-python to assemble a sequence of images into a video.
+The first image defines the frame size of the video, and the result video is stored as an AVI file.
+"""
+n_frames = 250  # Number of frames based on tEnd/dt
+img_template = "tmp/img_{:04d}.png"  # where to look for the images
+output_video = "video.avi"  # output name and filetype
 
 # Read first frame to get size
 frame = cv2.imread(img_template.format(0))
@@ -12,6 +17,7 @@ height, width, layers = frame.shape
 fourcc = cv2.VideoWriter_fourcc(*'DIVX')
 video = cv2.VideoWriter(output_video, fourcc, 15, (width, height))
 
+# Iterate through frames and write them to the video if they exist
 for i in range(n_frames):
     img = cv2.imread(img_template.format(i))
     if img is None:
